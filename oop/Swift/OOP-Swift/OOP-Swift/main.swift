8f4da193aeca5e3028f5614930b43f3491d5198b
Caio Dias <kiobreno@gmail.com>
2017-06-19 15:32:14 -0400

    Print the new baseFood field from each model

--- oop/Swift/OOP-Swift/OOP-Swift/main.swift
+++ oop/Swift/OOP-Swift/OOP-Swift/main.swift
@@ -14,19 +14,19 @@ print("⚠️ Swift does not have abstract option yet. However, is possible to m
 
 // Dog example
 let lessie = Dog()
-print("Dog Lessie Sound: \(lessie.getSound())")
+print("Dog Lessie Sound: \(lessie.getSound())\nFood: \(lessie.baseFood)")
 
 // Lion example
 let scar = Lion()
-print("Lion Scar Sound: \(scar.getSound())")
+print("Lion Scar Sound: \(scar.getSound())\nFood: \(scar.baseFood)")
 
 // Sheep example
 let dolly = Sheep()
-print("Sheep Dolly Sound: \(dolly.getSound())")
+print("Sheep Dolly Sound: \(dolly.getSound())\nFood: \(dolly.baseFood)")
 
 // Cat example
 let garfield = Sheep()
-print("Cat Garfield Sound: \(garfield.getSound())")
+print("Cat Garfield Sound: \(garfield.getSound())\nFood: \(garfield.baseFood)")
 
 print("\n")
 print("Using protocols, inheritance and objects we are able to plot different scenarios with a very few lines of code.\n")
