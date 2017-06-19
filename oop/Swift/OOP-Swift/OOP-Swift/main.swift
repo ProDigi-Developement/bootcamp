153e81c3f3d378fce87e9e0306655926ce91fd64
Caio Dias <kiobreno@gmail.com>
2017-06-19 15:50:03 -0400

    Display the food for each created animal.

--- oop/Swift/OOP-Swift/OOP-Swift/main.swift
+++ oop/Swift/OOP-Swift/OOP-Swift/main.swift
@@ -13,19 +13,19 @@ print("OOP Swift how NOT to do examples.\n")
 
 // Dog example
 let lessie = Animal(withType: .dog)
-print("Dog Lessie Sound: \(lessie.getSound())")
+print("Dog Lessie Sound: \(lessie.getSound())\nFood: \(lessie.baseFood)")
 
 // Lion example
 let scar = Animal(withType: .lion)
-print("Lion Scar Sound: \(scar.getSound())")
+print("Lion Scar Sound: \(scar.getSound())\nFood: \(scar.baseFood)")
 
 // Sheep example
 let dolly = Animal(withType: .sheep)
-print("Sheep Dolly Sound: \(dolly.getSound())")
+print("Sheep Dolly Sound: \(dolly.getSound())\nFood: \(dolly.baseFood)")
 
 // Cat example
 let garfield = Animal(withType: .cat)
-print("Cat Garfield Sound: \(garfield.getSound())")
+print("Cat Garfield Sound: \(garfield.getSound())\nFood: \(garfield.baseFood)")
 
 print("\n")
 print("Using a single class and putting everything on it is a mess.\n")
