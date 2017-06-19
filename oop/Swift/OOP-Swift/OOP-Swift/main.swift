af8017b93cb4703256dd1bf2b08a1909ed0015bd
Caio Dias <kiobreno@gmail.com>
2017-06-19 16:18:41 -0400

    Display the new property on printing.

--- oop/Swift/OOP-Swift/OOP-Swift/main.swift
+++ oop/Swift/OOP-Swift/OOP-Swift/main.swift
@@ -13,19 +13,19 @@ print("OOP Swift how NOT to do examples.\n")
 
 // Dog example
 let lessie = Animal(withType: .dog)
-print("Dog Lessie Sound: \(lessie.getSound())\nFood: \(lessie.baseFood)")
+print("Dog Lessie Sound: \(lessie.getSound())\nFood: \(lessie.baseFood)\nMax Age:\(lessie.maxAge)")
 
 // Lion example
 let scar = Animal(withType: .lion)
-print("Lion Scar Sound: \(scar.getSound())\nFood: \(scar.baseFood)")
+print("Lion Scar Sound: \(scar.getSound())\nFood: \(scar.baseFood)\nMax Age:\(lessie.maxAge)")
 
 // Sheep example
 let dolly = Animal(withType: .sheep)
-print("Sheep Dolly Sound: \(dolly.getSound())\nFood: \(dolly.baseFood)")
+print("Sheep Dolly Sound: \(dolly.getSound())\nFood: \(dolly.baseFood)\nMax Age:\(lessie.maxAge)")
 
 // Cat example
 let garfield = Animal(withType: .cat)
-print("Cat Garfield Sound: \(garfield.getSound())\nFood: \(garfield.baseFood)")
+print("Cat Garfield Sound: \(garfield.getSound())\nFood: \(garfield.baseFood)\nMax Age:\(lessie.maxAge)")
 
 print("\n")
 print("Using a single class and putting everything on it is a mess.\n")
