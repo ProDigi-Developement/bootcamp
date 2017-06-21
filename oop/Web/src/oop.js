ac2f5cbb561c45aaaf013987d86a34ec240da932
Tharshan <tharshan09@gmail.com>
2017-06-21 12:52:09 -0400

    commit changes

--- oop/Web/src/oop.js
+++ oop/Web/src/oop.js
@@ -1,36 +1,35 @@
 class Animal {
-    getSound() {
-        throw new TypeError('Abstract class cannot be instantiated directly.');  
+    constructor(animalType) {
+        self.animalType = animalType;
     }
-}
-
-class Dog extends Animal {
     getSound() {
-        return "Woof"
-    }
-}
-
-class Lion extends Animal {
-    getSound() {
-        return "Roar"
-    }
-}
-
-class Sheep extends Animal {
-    getSound() {
-        return "Baaa"
+        let sound = "";
+        switch (self.animalType) {
+            case "dog":
+                sound = "Woof";
+                break;
+            case "cat":
+                sound = "Meow";
+            case "lion":
+                sound = "Roar";
+            case "sheep":
+                sound = "Baaa";
+            default:
+                break;
+        }
+        return sound;
     }
 }
 
 class Example {
     constructor() {
         console.log("OOP Web examples.");
-        let lassie = new Dog();
+        let lassie = new Animal("dog");
         console.log(`Dog Lassie Sound: ${lassie.getSound()}`)
-        let scar = new Lion();
-        console.log(`Dog Lassie Sound: ${scar.getSound()}`)
-        let dolly = new Sheep();
-        console.log(`Dog Lassie Sound: ${dolly.getSound()}`)
+        let scar = new Animal("lion");
+        console.log(`Lion scar Sound: ${scar.getSound()}`)
+        let dolly = new Animal("sheep");
+        console.log(`Sheep dolly Sound: ${dolly.getSound()}`)
     }
 }
 
