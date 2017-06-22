550e186992c7c7e0831e726380265e0ed5d3195e
Caio Dias <kiobreno@gmail.com>
2017-06-22 18:07:30 -0400

    Add Swift CustomTableViewCell class

---
+++ oop/Swift/BaseProject/BaseProject/CustomTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CustomTableViewCell.swift
+//  BaseProject
+//
+//  Created by Caio Dias on 2017-06-22.
+//  Copyright © 2017 ProDigi. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+    
+}
