a719a32cf069437bf134f1be1f8d7a6fd7f561bf
Caio Dias <kiobreno@gmail.com>
2017-06-22 17:20:01 -0400

    Add the CustomController class.

---
+++ oop/Swift/BaseProject/BaseProject/CustomController.swift
@@ -0,0 +1,11 @@
+//
+//  CustomController.swift
+//  BaseProject
+//
+//  Created by Caio Dias on 2017-06-22.
+//  Copyright © 2017 ProDigi. All rights reserved.
+//
+
+final class CustomController {
+    public private(set) var list: [CustomObject] = [CustomObject]()
+}
