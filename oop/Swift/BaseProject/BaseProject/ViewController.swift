535af8e94dcb4467b2cafac7e275a22f232a6658
Caio Dias <kiobreno@gmail.com>
2017-06-22 16:16:41 -0400

    Initial commit for MVC Base Project.

---
+++ oop/Swift/BaseProject/BaseProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BaseProject
+//
+//  Created by Caio Dias on 2017-06-22.
+//  Copyright © 2017 ProDigi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
