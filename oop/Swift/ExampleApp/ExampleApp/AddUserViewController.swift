badbc6332d540b99e70dd453017a399b440c2ab9
Caio Dias <kiobreno@gmail.com>
2017-06-26 14:39:23 -0400

    Call the add new user on add button click.

--- oop/Swift/ExampleApp/ExampleApp/AddUserViewController.swift
+++ oop/Swift/ExampleApp/ExampleApp/AddUserViewController.swift
@@ -18,10 +18,22 @@ class AddUserViewController: UIViewController {
 
     @IBAction func addButtonTouchUpInside(_ sender: Any) {
         if validateInput() {
+            let newUser = User(withName: self.textFieldName.text!, description: self.textFieldDescription.text!)
+            UserController.shared.addUser(newUser, onSuccess: addSuccess, onFail: addFail)
         }
     }
     
     private func validateInput() -> Bool {
         return true
+    }
+    
+    // MARK: Callbacks for add new user
+    
+    private func addSuccess() {
+        Util.displayAlert(onView: self, withTitle: "Success", message: "New user added successfully.")
+    }
+    
+    private func addFail(errorMessage: String) {
+        Util.displayAlert(onView: self, withTitle: "Fail", message: "New user not added.")
     }
 }
