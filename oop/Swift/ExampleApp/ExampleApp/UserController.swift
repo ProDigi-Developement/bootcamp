24f455816cd389c1f378b624ca947c03f30d0a77
Caio Dias <kiobreno@gmail.com>
2017-07-05 19:49:16 -0400

    Separate token

--- oop/Swift/ExampleApp/ExampleApp/UserController.swift
+++ oop/Swift/ExampleApp/ExampleApp/UserController.swift
@@ -15,6 +15,7 @@ public typealias ControllerFailScenario = (String) -> Void
 public final class UserController {
     public private(set) var userList: [User]
     public var delegate: FetchDelegate? = nil
+    private var token: String = "eyJhbGciOiJSUzI1NiIsImtpZCI6IjJiYjY5NTczZjAzMDUyOTdkNDc0MDQ1OWE0YzA2N2Q1NGI1YmRmMjkifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vcHJvZGlnaS1ib290Y2FtcCIsImF1ZCI6InByb2RpZ2ktYm9vdGNhbXAiLCJhdXRoX3RpbWUiOjE0OTkyOTU1ODksInVzZXJfaWQiOiJCRU4yWmx6WGJwYlFadmlPdVhLdlFkeWJDMXYxIiwic3ViIjoiQkVOMlpselhicGJRWnZpT3VYS3ZRZHliQzF2MSIsImlhdCI6MTQ5OTI5NTU5MCwiZXhwIjoxNDk5Mjk5MTkwLCJlbWFpbCI6Imtpb2JyZW5vK3Byb2RpZ2lAZ21haWwuY29tIiwiZW1haWxfdmVyaWZpZWQiOmZhbHNlLCJmaXJlYmFzZSI6eyJpZGVudGl0aWVzIjp7ImVtYWlsIjpbImtpb2JyZW5vK3Byb2RpZ2lAZ21haWwuY29tIl19LCJzaWduX2luX3Byb3ZpZGVyIjoicGFzc3dvcmQifX0.XipTw72pkB8vQy9KrbDp5nbkl1bqZpp-zUaPANJr6R2AxlL-q6-zYqAcPtwNk1LwnWLeorgnd4M4QMMowNNS50Qi2ia4KPechdbsUuIIjdNbergKqD-qfPR2oWU7L5HWk-sqM2t5Mzql-rC2LITP6fVD5K91GLVSPcuvjk5fO3-WzSTW8Sg-pk642QTiBO8HX66atq7BG8HE3boHFUJkBGI7COSMIpTum7B3U5z8J0QIGF3b9YuBDLFmEPu156QEJoh3Xm24KYo9Izr1EqCSt6zEpXOHhBbePALqHSrSfClcQpe2hJpI3orS1XVVrjT-ZITvUr8zh2w1MhOkZhgeeQ"
     
     // MARK: Singleton
     
@@ -37,16 +38,12 @@ public final class UserController {
     // MARK: Fetch users from backend (Firebase)
     
     public func fetchUsers() {
-        let url: String = "https://prodigi-bootcamp.firebaseio.com/BEN2ZlzXbpbQZviOuXKvQdybC1v1/messages.json?auth=eyJhbGciOiJSUzI1NiIsImtpZCI6IjJiYjY5NTczZjAzMDUyOTdkNDc0MDQ1OWE0YzA2N2Q1NGI1YmRmMjkifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vcHJvZGlnaS1ib290Y2FtcCIsImF1ZCI6InByb2RpZ2ktYm9vdGNhbXAiLCJhdXRoX3RpbWUiOjE0OTkyOTU1ODksInVzZXJfaWQiOiJCRU4yWmx6WGJwYlFadmlPdVhLdlFkeWJDMXYxIiwic3ViIjoiQkVOMlpselhicGJRWnZpT3VYS3ZRZHliQzF2MSIsImlhdCI6MTQ5OTI5NTU5MCwiZXhwIjoxNDk5Mjk5MTkwLCJlbWFpbCI6Imtpb2JyZW5vK3Byb2RpZ2lAZ21haWwuY29tIiwiZW1haWxfdmVyaWZpZWQiOmZhbHNlLCJmaXJlYmFzZSI6eyJpZGVudGl0aWVzIjp7ImVtYWlsIjpbImtpb2JyZW5vK3Byb2RpZ2lAZ21haWwuY29tIl19LCJzaWduX2luX3Byb3ZpZGVyIjoicGFzc3dvcmQifX0.XipTw72pkB8vQy9KrbDp5nbkl1bqZpp-zUaPANJr6R2AxlL-q6-zYqAcPtwNk1LwnWLeorgnd4M4QMMowNNS50Qi2ia4KPechdbsUuIIjdNbergKqD-qfPR2oWU7L5HWk-sqM2t5Mzql-rC2LITP6fVD5K91GLVSPcuvjk5fO3-WzSTW8Sg-pk642QTiBO8HX66atq7BG8HE3boHFUJkBGI7COSMIpTum7B3U5z8J0QIGF3b9YuBDLFmEPu156QEJoh3Xm24KYo9Izr1EqCSt6zEpXOHhBbePALqHSrSfClcQpe2hJpI3orS1XVVrjT-ZITvUr8zh2w1MhOkZhgeeQ"
+        let url: String = "https://prodigi-bootcamp.firebaseio.com/BEN2ZlzXbpbQZviOuXKvQdybC1v1/messages.json?auth=\(self.token)"
 
         guard let urlRequest = URL(string: url) else {
             print("Not possible to create the URL object")
             return
         }
-        
-//        var request = URLRequest(url: urlRequest)
-//        request.httpMethod = ""
-//        request.httpBody = Data()
         
         let config = URLSessionConfiguration.default
         let session = URLSession(configuration: config)
@@ -77,8 +74,7 @@ public final class UserController {
     
     public func addUser(_ user: User, onSuccess: @escaping ControllerSuccessScenario, onFail: @escaping ControllerFailScenario) {
         
-        
-        let url: String = "https://prodigi-bootcamp.firebaseio.com/BEN2ZlzXbpbQZviOuXKvQdybC1v1/messages.json?auth=eyJhbGciOiJSUzI1NiIsImtpZCI6IjJiYjY5NTczZjAzMDUyOTdkNDc0MDQ1OWE0YzA2N2Q1NGI1YmRmMjkifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vcHJvZGlnaS1ib290Y2FtcCIsImF1ZCI6InByb2RpZ2ktYm9vdGNhbXAiLCJhdXRoX3RpbWUiOjE0OTkyOTU1ODksInVzZXJfaWQiOiJCRU4yWmx6WGJwYlFadmlPdVhLdlFkeWJDMXYxIiwic3ViIjoiQkVOMlpselhicGJRWnZpT3VYS3ZRZHliQzF2MSIsImlhdCI6MTQ5OTI5NTU5MCwiZXhwIjoxNDk5Mjk5MTkwLCJlbWFpbCI6Imtpb2JyZW5vK3Byb2RpZ2lAZ21haWwuY29tIiwiZW1haWxfdmVyaWZpZWQiOmZhbHNlLCJmaXJlYmFzZSI6eyJpZGVudGl0aWVzIjp7ImVtYWlsIjpbImtpb2JyZW5vK3Byb2RpZ2lAZ21haWwuY29tIl19LCJzaWduX2luX3Byb3ZpZGVyIjoicGFzc3dvcmQifX0.XipTw72pkB8vQy9KrbDp5nbkl1bqZpp-zUaPANJr6R2AxlL-q6-zYqAcPtwNk1LwnWLeorgnd4M4QMMowNNS50Qi2ia4KPechdbsUuIIjdNbergKqD-qfPR2oWU7L5HWk-sqM2t5Mzql-rC2LITP6fVD5K91GLVSPcuvjk5fO3-WzSTW8Sg-pk642QTiBO8HX66atq7BG8HE3boHFUJkBGI7COSMIpTum7B3U5z8J0QIGF3b9YuBDLFmEPu156QEJoh3Xm24KYo9Izr1EqCSt6zEpXOHhBbePALqHSrSfClcQpe2hJpI3orS1XVVrjT-ZITvUr8zh2w1MhOkZhgeeQ"
+        let url: String = "https://prodigi-bootcamp.firebaseio.com/BEN2ZlzXbpbQZviOuXKvQdybC1v1/messages.json?auth=\(self.token)"
         
         guard let urlRequest = URL(string: url) else {
             print("Not possible to create the URL object")
@@ -131,7 +127,7 @@ public final class UserController {
     // MARK: Delete user on backend (Firebase)
     
     public func deleteUser(_ user: User, onSuccess: @escaping ControllerSuccessScenario, onFail: @escaping ControllerFailScenario) {
-        let stringFormat: String = "https://prodigi-bootcamp.firebaseio.com/BEN2ZlzXbpbQZviOuXKvQdybC1v1/messages/%@.json?auth=eyJhbGciOiJSUzI1NiIsImtpZCI6IjJiYjY5NTczZjAzMDUyOTdkNDc0MDQ1OWE0YzA2N2Q1NGI1YmRmMjkifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vcHJvZGlnaS1ib290Y2FtcCIsImF1ZCI6InByb2RpZ2ktYm9vdGNhbXAiLCJhdXRoX3RpbWUiOjE0OTkyOTU1ODksInVzZXJfaWQiOiJCRU4yWmx6WGJwYlFadmlPdVhLdlFkeWJDMXYxIiwic3ViIjoiQkVOMlpselhicGJRWnZpT3VYS3ZRZHliQzF2MSIsImlhdCI6MTQ5OTI5NTU5MCwiZXhwIjoxNDk5Mjk5MTkwLCJlbWFpbCI6Imtpb2JyZW5vK3Byb2RpZ2lAZ21haWwuY29tIiwiZW1haWxfdmVyaWZpZWQiOmZhbHNlLCJmaXJlYmFzZSI6eyJpZGVudGl0aWVzIjp7ImVtYWlsIjpbImtpb2JyZW5vK3Byb2RpZ2lAZ21haWwuY29tIl19LCJzaWduX2luX3Byb3ZpZGVyIjoicGFzc3dvcmQifX0.XipTw72pkB8vQy9KrbDp5nbkl1bqZpp-zUaPANJr6R2AxlL-q6-zYqAcPtwNk1LwnWLeorgnd4M4QMMowNNS50Qi2ia4KPechdbsUuIIjdNbergKqD-qfPR2oWU7L5HWk-sqM2t5Mzql-rC2LITP6fVD5K91GLVSPcuvjk5fO3-WzSTW8Sg-pk642QTiBO8HX66atq7BG8HE3boHFUJkBGI7COSMIpTum7B3U5z8J0QIGF3b9YuBDLFmEPu156QEJoh3Xm24KYo9Izr1EqCSt6zEpXOHhBbePALqHSrSfClcQpe2hJpI3orS1XVVrjT-ZITvUr8zh2w1MhOkZhgeeQ"
+        let stringFormat: String = "https://prodigi-bootcamp.firebaseio.com/BEN2ZlzXbpbQZviOuXKvQdybC1v1/messages/%@.json?auth=\(self.token)"
         
         let url: String = String(format: stringFormat, user.objectId)
         
